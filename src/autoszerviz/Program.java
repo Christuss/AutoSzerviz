605b844e6bef632ccb6b4e4c4e5b14bbda77570d
Christuss <szendefi.krisztofer@gmail.com>
2023-11-20 15:43:08 +0100

    program felkesz

--- src/autoszerviz/Program.java
+++ src/autoszerviz/Program.java
@@ -14,7 +14,23 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        new Program().program();
     }
     
+    public void program() {
+        Szerviz s = new Szerviz(5);
+        SzemelyAuto sa1 = new SzemelyAuto();
+        SzemelyAuto sa2 = new SzemelyAuto();
+        TeherAuto ta1 = new TeherAuto();
+        SzemelyAuto sa3 = new SzemelyAuto();
+        s.betesz(sa1);
+        s.betesz(sa2);
+        s.betesz(sa3);
+        s.betesz(ta1);
+        
+        System.out.println(s.toString());
+        sa1.mozgas();
+        sa1.mozgas();
+        sa1.mozgas();
+    }
 }
